556cb41d78365a434289d09f09e302562a1b5776
PatrickLin99 <61694830+PatrickLin99@users.noreply.github.com>
2020-05-10 15:59:05 +0800

    1st upload

---
+++ andfun-kotlin-mars-real-estate-Step.08-Solution-Adding-a-Filter_v06/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate
+
+import android.view.View
+import android.widget.ActionMenuView
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
+import java.text.DecimalFormat
+
+/**
+ * When there is no Mars property data (data is null), hide the [RecyclerView], otherwise show it.
+ */
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?) {
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.addHeaderAndSubmitList(data)
+}
+
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+                .load(imgUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image))
+                .into(imgView)
+    }
+}
+
+@BindingAdapter("typeUrl")
+fun bindtype(typeView: TextView, typeUrl: String?){
+    typeUrl?.let {
+        val typeUri = typeUrl.toUri().buildUpon().build()
+        var type = typeUri.toString()
+        if (type == "buy") {
+            type == "Sale"
+        } else {
+            type == "Rent"
+        }
+        typeView.text = "For " + type
+    }
+}
+
+@BindingAdapter("mUrl")
+fun bindmonth(mView: TextView, mUrl: String?){
+    mUrl?.let{
+        val mUri = mUrl.toUri().buildUpon().build()
+        val type = mUri.toString()
+        if (type == "rent") {
+            mView.text = " /month"
+        } else {
+            mView.text = " "
+        }
+    }
+}
+
+@BindingAdapter("priceUrl")
+fun TextView.bindprice(priceUrl:Double){
+    priceUrl?.let{
+        var priceView = this.findViewById(R.id.price_value_text) as TextView
+        var tag = DecimalFormat("$,###")
+        var price = tag.format(priceUrl)
+        priceView.text = price
+    }
+}
+
+
+
+/**
+ * This binding adapter displays the [MarsApiStatus] of the network request in an image view.  When
+ * the request is loading, it displays a loading_animation.  If the request has an error, it
+ * displays a broken image to reflect the connection error.  When the request is finished, it
+ * hides the image view.
+ */
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+    }
+}
